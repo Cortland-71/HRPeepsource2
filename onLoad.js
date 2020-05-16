8b9f6906ae764fb75d87c709f7e0fc6f19371b51
Cortland <cortland.carrillo.dev@gmail.com>
2020-05-16 15:53:13 -0700

    Added table

---
+++ onLoad.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    var navList = ['#detailDiv', '#performanceDiv', '#contactsDiv',
+        '#wagesDiv', '#taxesDiv', '#emergancyDiv', '#seperationsDiv', '#eeoDiv'];
+
+    var numOfNavBtn = 9;
+
+    $(".search-section div").hide().slideDown(500);
+
+    for (var i = 0; i < numOfNavBtn; i++) {
+        $("#" + i).hide();
+        $("#" + i).fadeIn((i + 1) * 80);
+    }
+
+    $(".navBtn").on('click', function () {
+        var index = $(this).attr("id");
+        var selectedDiv = navList[index];
+        $(selectedDiv).show();
+        for (var i = 0; i < numOfNavBtn; i++) {
+            if (selectedDiv != navList[i]) {
+                $(navList[i]).hide();
+            }
+        }
+    });
+});
